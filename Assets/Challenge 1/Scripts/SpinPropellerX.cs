94b0892568a61216ca93fd5e3a57aa9de1ea6533
joseadolfomartinezolvera <56425103+joseadolfomartinezolvera@users.noreply.github.com>
2022-10-04 20:33:46 -0500

    Update SpinPropellerX.cs

--- Assets/Challenge 1/Scripts/SpinPropellerX.cs
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -11,9 +11,10 @@ public class SpinPropellerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    private float propellorSpeed = 1000;
+    private float propellorSpeed = 1000; //variable que se encarga de la velocidad a la que gira el helice 
     void Update()
     {
+    //parte que se encarga de hacer girar el helice 
         transform.Rotate(Vector3.forward, propellorSpeed * Time.deltaTime);
     }
 
